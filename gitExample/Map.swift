2d984c62497740ccf868b4c919ca8ff92c56b45b
garykueh <garykueh@gmail.com>
2020-07-02 17:41:38 -0700

    Add map

---
+++ gitExample/Map.swift
@@ -0,0 +1,15 @@
+//
+//  Map.swift
+//  gitExample
+//
+//  Created by Gp on 7/2/20.
+//  Copyright © 2020 Gp. All rights reserved.
+//
+
+import UIKit
+
+class Map: NSObject {
+    func sayHello() {
+      print("Hello from MapForItinerary")
+    }
+}
